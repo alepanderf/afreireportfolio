84f595fb6ef98acfe15b5d7ab3a132c5f31e97e7
Alejandro Freire <alejandrofreire@Alejandros-MacBook-Air.local>
2024-11-21 11:34:25 -0500

    take 3

--- src/components/card/index.js
+++ src/components/card/index.js
@@ -2,12 +2,14 @@ import './index.scss';
 
 const Card = ({ image, name, description, githubLink }) => {
     return (
-        <div className="card">
-            <img>{image}</img>
-            <p>{name}</p>
-            <p>{description}</p>
-            <p>{githubLink}</p>
-        </div>
+        <span>
+            <div className="card">
+                <img>{image}</img>
+                <p>{name}</p>
+                <p>{description}</p>
+                <p>{githubLink}</p>
+            </div>
+        </span>
     )
 }
 
