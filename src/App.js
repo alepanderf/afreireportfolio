b522e62e9dbdd264d146d1f87d1641d19ad61a89
Alejandro Freire <alejandrofreire@Alejandros-MacBook-Air.local>
2025-02-13 13:26:17 -0500

    remove projects page

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Layout from './components/layout'
 import Home from './components/home'
 import About from './components/about'
 import Contact from './components/contact'
-import Projects from './components/projects'
 
 function App() {
   return (
@@ -14,7 +13,6 @@ function App() {
         <Route index element={<Home />} />
         <Route path="about" element={<About />} />
         <Route path="contact" element={<Contact />} />
-        <Route path="projects" element={<Projects />} />
       </Route>
     </Routes>
     </>
