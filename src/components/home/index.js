b183c9dd68c1dae6b8d9cc39ffbe6bed2ddec7c1
Alejandro Freire <alejandrofreire@Alejandros-MacBook-Air.local>
2025-02-13 19:30:11 -0500

    deleted clutter from home page

--- src/components/home/index.js
+++ src/components/home/index.js
@@ -3,13 +3,11 @@ import './index.scss';
 import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import AnimatedLetters from '../AnimatedLetters';
-import Logo from './logo';
 import Loader from 'react-loaders';
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
     const nameArray = ['l','e','j','a','n','d','r','o']
-    const jobArray = ['S','o','f','t','w','a','r','e',' ','E','n','g','i','n','e','e','r']
     
     useEffect(() => {
         const timeoutId = setTimeout(() => {
@@ -35,9 +33,6 @@ const Home = () => {
                 strArray={nameArray}
                 idx={15}/>
                 <br/>
-                <AnimatedLetters letterClass={letterClass}
-                strArray={jobArray}
-                idx={22}/>
                 </h1>
                 <Link to="/contact" className='flat-button'>
                     CONTACT ME
